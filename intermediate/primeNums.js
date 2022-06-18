5ac6032a6111b0028be7302dc4d0e1c19f88f573
SylviahM <sylviahcheroposailenyi@gmail.com>
2022-06-18 19:58:22 +0300

    prime numbers commit

---
+++ intermediate/primeNums.js
@@ -0,0 +1,11 @@
+function isPrime(num){
+    let a = 0;
+    for(let i = 0; i < num; i++){
+        if(num % i === 0)
+        console.log("is not prime");
+
+    }
+        console.log("is prime");
+}
+isPrime(11 % 4);
+isPrime(20 % 3);
